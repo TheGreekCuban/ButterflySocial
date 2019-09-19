abbfaf1217d207ebe9649b91cb951d6202b48af1
TheGreekCuban <theodorealexander92@gmaill.com>
2019-09-18 20:11:14 -0400

    backend stream code done

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavigationBar from "./components/Nav";
-import Wrapper from "./components/Wrapper";
-import SignupPage from "./components/SignupPage";
+import NavigationBar from "./components/Pages/Nav";
+import Wrapper from "./components/Pages/Wrapper";
+import SignupPage from "./components/Pages/SignupPage";
+import Streams from "./components/Pages/Streams";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <NavigationBar />
       <Switch>
         <Route exact path="/register" render={(props) => <SignupPage {...props} title={'WELCOME TO BUTTERFLY'} />} />
+        <Route exact path="/api/streams" render={(props) => <Streams {...props} title={'SEE ALL STREAMS BELOW'} />} />
       </Switch>
       </Wrapper>
     </Router>
