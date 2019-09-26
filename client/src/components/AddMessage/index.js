47626ba18224cecc433c0c893b7504f804fd1577
Vidhi Vaks <vidhi.vakharia93@gmail.com>
2019-09-25 20:43:38 -0400

    added button in the modal

--- client/src/components/AddMessage/index.js
+++ client/src/components/AddMessage/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
 
 function AddMessage() {
   const [show, setShow] = React.useState(false);
@@ -11,20 +12,27 @@ function AddMessage() {
   return (
     <div>
       <Button variant="primary" onClick={handleShow}>
-        Add new message
+        Let's Talk About It
       </Button>
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Message</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="exampleForm.ControlTextarea1">
+              <Form.Label>Write text here</Form.Label>
+              <Form.Control as="textarea" rows="3" />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
           <Button variant="primary" onClick={handleClose}>
-            Save Changes
+            Send
           </Button>
         </Modal.Footer>
       </Modal>
